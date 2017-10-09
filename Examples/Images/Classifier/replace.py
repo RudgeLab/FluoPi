a5fe4c86d1a20b531594f01ba48b6fa4cbe68b91
FernanFederici <ffederici@bio.puc.cl>
2017-10-08 23:05:52 -0300

    replace 200 for lim2 in classiffier

---
+++ Examples/Images/Classifier/replace.py
@@ -0,0 +1,12 @@
+import os
+
+def replace(fpath, old_str, new_str):
+    for path, subdirs, files in os.walk(fpath):
+        for name in files:
+            if(old_str.lower() in name.lower()):
+                os.rename(os.path.join(path,name), os.path.join(path,
+                                            name.lower().replace(old_str,new_str)))
+replace('/Users/fernanfederici/Dropbox/000_hardware/DIY_equip_2016/FluoPi/Examples/Images/Classifier/BeRFP_data', '*.jpg', 'image_(%s).jpg')
+
+
+
