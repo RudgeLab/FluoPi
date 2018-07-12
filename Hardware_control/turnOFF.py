4686b537b8da6d07af8cace4eff935786809291c
Prosimio <innunez@uc.cl>
2018-07-12 15:10:12 -0400

    it were eliminated some unnecessary lines and GPIO number was added as an input option

--- Hardware_control/turnOFF.py
+++ Hardware_control/turnOFF.py
@@ -1,9 +1,21 @@
-from picamera import PiCamera
+#to run: python turnOFF.py GPIO_number
+#where GPIO_number have to be an integer. e.g. 29
+
 import RPi.GPIO as GPIO
+import sys
 
+# Set the GPIO number where LEDs control is conected
+if len(sys.argv)==2:
+    GPIO_num = int(sys.argv[1])            # e.g. Timelapse
+
+else:
+    GPIO_num = 29   #GPIO 29 is used as the default
+
+GPIO.setwarnings(False)
+    
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(29, GPIO.OUT)
+GPIO.setup(GPIO_num, GPIO.OUT)
 
-GPIO.output(29,GPIO.LOW)
+GPIO.output(GPIO_num,GPIO.LOW)
 
 GPIO.cleanup()
